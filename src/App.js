ff176ce382cadea4bb363b84da3c0528d926b535
UcheSylvester <okorocode@gmail.com>
2020-01-21 00:08:46 +0100

    getting route props using withRouter

--- src/App.js
+++ src/App.js
@@ -15,44 +15,14 @@ const HatsPage = (props) => {
   )
 }
 
-const Topics = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Link to={`${props.match.url}/13`}>Topic 13</Link>
-      <Link to={`${props.match.url}/15`}>Topic 15</Link>
-      <Link to={`${props.match.url}/18`}>Topic 18</Link>
-
-      <h1>TOPICS PAGE</h1>
-    </div>
-  )
-}
-
-const TopicsDetails = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Link to="/">Home</Link>
-      <h1>TOPIC DETAIL PAGE : {props.match.params.topicId} </h1>
-    </div>
-  )
-}
-
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
-
-      {/* <Switch>
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/hats" component={HatsPage} />
-      </Switch> */}
-
-      <Route exact path="/" component={HatsPage} />
-      <Route exact path="/topics" component={Topics} />
-      <Route path="/topics/:topicId" component={TopicsDetails} />
-
+      </Switch>
     </div>
   );
 }
