c3a2848431c0f4518c264f5844aa26cc8451ea65
UcheSylvester <okorocode@gmail.com>
2020-01-21 22:56:22 +0100

    add comment

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, ...otherProps }) => {
-  console.log(children, otherProps);
-  return (
-    <button className="custom-button" {...otherProps}>
-      {children}
-    </button>
-  );
-};
+const CustomButton = ({ children, ...otherProps }) => (
+  // children is the text typed within the CustomButton component
+  // ...otherProps refers to other properties for the where the CustomButton is used
+  // in a object using the rest operator
+  <button className="custom-button" {...otherProps}>
+    {children}
+  </button>
+);
 
 export default CustomButton;
