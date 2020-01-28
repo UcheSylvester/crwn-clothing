064b79a28bd33e0cde92c8a37b6b976bbdcefe71
UcheSylvester <okorocode@gmail.com>
2020-01-28 11:10:29 +0100

    displaying collection overview for nexted routing

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
@@ -9,15 +10,15 @@ import { selectShopCollections } from "../../redux/shop/shop.selector";
 import "./collection-overview.styles.scss";
 
 const CollectionOverview = ({ collections }) => (
-  <div className="collection-overview">
+  <div className="collections-overview">
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
   </div>
 );
 
-const mapStateToProps = state => ({
-  collections: selectShopCollections(state)
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-import { selectShopCollections } from "../../redux/shop/shop.selector";
 
 import "./shop.styles.scss";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview collections={collections} />
   </div>
 );
 
-const mapStateToProps = state => ({
-  collections: selectShopCollections(state)
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
