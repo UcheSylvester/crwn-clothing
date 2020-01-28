68045783c4436f6a4a7010e9c36937631f2e3189
UcheSylvester <okorocode@gmail.com>
2020-01-28 20:51:11 +0100

    fix: removed exact from collectionPage route to navigate to nested routes

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
+
 import "./collection.styles.scss";
 
-const collectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ match, collection }) => {
+  console.log(collection, match, "dldl");
+
+  const { title, items } = collection;
+
   return (
-    <div className="collection">
-      <h2>COLLECTION PAGE</h2>
+    <div className="collection-page">
+      <h2 className="title"> {title} </h2>
+
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default collectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,13 +4,14 @@ import { Route } from "react-router-dom";
 
 import "./shop.styles.scss";
 
-import collectionPage from "../collection/collection.component";
+import CollectionPage from "../collection/collection.component";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={collectionPage} />
+
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
